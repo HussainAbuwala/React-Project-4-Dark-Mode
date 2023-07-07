14a22d4e989ab9d2e6ecaf450f027e9c22b4e19a
Hussain <hussainabuwala.1997@gmail.com>
2023-07-07 10:24:15 -0400

    added the App Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
 
 function App() {
+
+  const [darkMode, setDarkMode] = React.useState(true)
+
+  function toggleDarkMode(){
+    setDarkMode(prevMode => !prevMode)
+  }
+
   return (
-    <div className="App">
-      <h1>Hello World !</h1>
+    <div className="container">
+      <Navbar
+        darkMode={darkMode}
+        toggleDarkMode={toggleDarkMode}
+      />
+      <Main darkMode={darkMode} />
     </div>
   );
 }
